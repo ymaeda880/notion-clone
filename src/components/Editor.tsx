601066fc146fb6145248068380adf45bd0ca1257
Yasuo <yasuomaeda2000@yahoo.co.jp>
2025-08-05 15:54:31 +0900

    initial commit

---
+++ src/components/Editor.tsx
@@ -0,0 +1,32 @@
+// import { BlockNoteEditor, PartialBlock, locales } from '@blocknote/core';
+import { useCreateBlockNote } from '@blocknote/react';
+import '@blocknote/mantine/style.css';
+import { BlockNoteView } from '@blocknote/mantine';
+
+import { ja } from '@blocknote/core/locales';
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string | null;
+}
+
+function Editor({ onChange, initialContent }: EditorProps) {
+  // const editor = useCreateBlockNote({ dictionary: locales.ja });
+
+  const editor = useCreateBlockNote({
+    dictionary: ja,
+    initialContent:
+      initialContent != null ? JSON.parse(initialContent) : undefined,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => onChange(JSON.stringify(editor.document))}
+      />
+    </div>
+  );
+}
+
+export default Editor;
